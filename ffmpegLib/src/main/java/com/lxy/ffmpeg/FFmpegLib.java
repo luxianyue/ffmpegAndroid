7ad948e38c1fcf6c649440d109046eb53f756a29
luxianyue <luxianyue666@outlook.com>
2024-09-20 17:20:41 +0800

    Add files via upload
    replace FFmpegLib.java

--- ffmpegLib/src/main/java/com/lxy/ffmpeg/FFmpegLib.java
+++ ffmpegLib/src/main/java/com/lxy/ffmpeg/FFmpegLib.java
@@ -2,6 +2,9 @@ package com.lxy.ffmpeg;
 
 import android.graphics.Bitmap;
 
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+
 
 public class FFmpegLib {
 
@@ -17,13 +20,27 @@ public class FFmpegLib {
         System.loadLibrary("avutil");
         System.loadLibrary("swresample");
         System.loadLibrary("swscale");
+        //System.loadLibrary("avresample");
     }
 
     public static native String getVersion();
 
+    public static native VFrame getVideoInfo(String path);
+    public static native VFrame getVideoFrame(String path);
+
     public static native Bitmap getVideoImage(String path);
 
     public static native int exeCmd(String[] cmd);
+
+    public static Bitmap getVideoBitmap(String path) {
+        VFrame vFrame = getVideoFrame(path);
+        if (vFrame != null) {
+            Bitmap bitmap = Bitmap.createBitmap(vFrame.width, vFrame.height, Bitmap.Config.ARGB_8888);
+            bitmap.copyPixelsFromBuffer(ByteBuffer.wrap(vFrame.frame));
+            return bitmap;
+        }
+        return null;
+    }
 
     public static int exeCmd(String cmd) {
         return exeCmd(cmd.split("\\s+"));
